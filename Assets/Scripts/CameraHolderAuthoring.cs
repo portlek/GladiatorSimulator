2c5a571e025a984cea74548233c39fa6ba17a2ba
Hasan Demirtaş <utsukushihito@outlook.com>
2025-03-13 01:32:15 +0300

    move to rot.

--- Assets/Scripts/CameraHolderAuthoring.cs
+++ Assets/Scripts/CameraHolderAuthoring.cs
@@ -1,7 +1,7 @@
 ﻿using Unity.Entities;
 using UnityEngine;
 
-namespace GladiatorSimulator.Authoring {
+namespace GladiatorSimulator {
 
 public class CameraHolderAuthoring : MonoBehaviour {
     public GameObject cameraObject;
