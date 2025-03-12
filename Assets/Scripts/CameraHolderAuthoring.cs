4d77aaabf50b8db41b5875d1ca7bc51012ccf762
Hasan Demirtaş <utsukushihito@outlook.com>
2025-03-13 01:04:28 +0300

    continue.

---
+++ Assets/Scripts/CameraHolderAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace GladiatorSimulator;
+
+public class CameraHolderAuthoring : MonoBehaviour {
+    public GameObject cameraObject;
+}
+
+public struct CameraHolderComponent : IComponentData {
+    public Entity CameraEntity;
+}
+
+public class CameraHolderAuthoringBaker : Baker<CameraHolderAuthoring> {
+    public override void Bake(CameraHolderAuthoring authoring) {
+        var entity = GetEntity(TransformUsageFlags.Dynamic);
+        AddComponent(entity, new CameraHolderComponent {
+            CameraEntity = GetEntity(authoring.cameraObject, TransformUsageFlags.Dynamic)
+        });
+    }
+}
