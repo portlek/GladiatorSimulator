a9451c1eb8f4ecb2dd190f7ed6d96598807c5cdd
Hasan Demirtaş <utsukushihito@outlook.com>
2025-03-14 09:30:17 +0300

    Update InputSystem.cs

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -5,6 +5,23 @@ namespace GladiatorSimulator {
 public class InputSystem : MonoBehaviour {
     public Vector2 Movement { get; private set; }
 
+    #region Player
+
+    #region Move
+    private bool IsPlayerMoveEnabled() => _inputs.Player.Move.enabled;
+
+    public void EnablePlayerMove() {
+        _inputs.Player.Move.Enable();
+    }
+
+    public void DisablePayerMove() {
+        _inputs.Player.Move.Disable();
+    }
+    #endregion
+
+    #endregion
+
+    #region Internal
     private Inputs _inputs;
 
     private void Awake() {
@@ -14,5 +31,10 @@ public class InputSystem : MonoBehaviour {
     private void Update() {
         Movement = _inputs.Player.Move.ReadValue<Vector2>();
     }
+
+    private void OnDisable() {
+        _inputs.Disable();
+    }
+    #endregion
 }
 }
